7a053feb72afa99e39ebace93a2c546b7f4ee50a
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-12 12:35:07 +0530

    Added Angular Notifier

--- angular-testing-lab/src/app/app.module.ts
+++ angular-testing-lab/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadImgTestComponent } from './components/upload-img-test/upload-img-test.component';
+import { AngularNotifierTestComponent } from './new/angular-notifier-test/angular-notifier-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadImgTestComponent
+    UploadImgTestComponent,
+    AngularNotifierTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-testing-lab/src/app/new/angular-notifier-test/angular-notifier-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-notifier-test',
+  templateUrl: './angular-notifier-test.component.html',
+  styleUrls: ['./angular-notifier-test.component.css']
+})
+export class AngularNotifierTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
