6f23a05cebd27f23b155444a10399685bee30aa8
ankit <ac2000choudhary@gmail.com>
2021-05-12 17:40:08 +0530

    Image Uploading Testing at Local Storage

--- Backend-server-Testing-Lab/index.js
+++ Backend-server-Testing-Lab/index.js
@@ -1,11 +1,19 @@
+
 var express = require('express');
 var app = express();
 var port = 3000;
+var cors = require('cors');
 
+// app.use(cors);
 
+app.post('/upload',(req, res)=>{
+    res.json({
+        'message': 'File uploaded successfully'
+    });
+});
 
 
 
 app.listen(port, ()=>{
-    console.log('listening on port')
+    console.log('listening on port ' + port);
 });
