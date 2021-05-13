47980c6b97716dba7bb6c261ccabe36524ea7036
ankit <ac2000choudhary@gmail.com>
2021-05-13 16:35:16 +0530

    Upload file to cloud Storage

--- Backend-server-Testing-Lab/index.js
+++ Backend-server-Testing-Lab/index.js
@@ -5,20 +5,42 @@ var port = 3000;
 var cors = require('cors');
 const multipart = require('connect-multiparty');
 const multipartMiddleware = multipart({
-    uploadDir: './upload'
+    storage: 'vedik'
 });
+
+var azure = require('azure-storage');
+var blobStorage = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=vedikstorage;AccountKey=7OmeGweYiFuBYd+/nzBAj3chFHo0vycwFc95s2yfbYpDOEbVPEnHY1lI5KWZms2G7O1//8F1JmGt2jcueom8jg==;EndpointSuffix=core.windows.net");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+// For Cloud Storage
 app.post('/upload',multipartMiddleware,(req, res)=>{
     console.log(req.body)
-    res.json({
-        'message': 'File uploaded successfully.'
+
+    blobStorage.createBlockBlobFromLocalFile('imagecontainer', req.files.file.name , req.files.file.path, function(err, result, response){
+        if(!err){
+            res.send({
+                'message': 'File uploaded successfully.'
+            })
+        }
+        else{
+            console.log(err);
+        }
     });
-    console.log(req.files.file.path);
+
+    
 });
+
+
+// app.post('/upload',multipartMiddleware,(req, res)=>{
+//     console.log(req.body)
+//     res.json({
+//         'message': 'File uploaded successfully.'
+//     });
+//     console.log(req.files.file.path);
+// });
 
 app.get('/upload', (req, res) => {
     res.json({
