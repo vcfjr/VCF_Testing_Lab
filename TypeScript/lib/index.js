14c6459e6b4d725217a3b5d2b236a769b4cd3974
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-19 15:43:42 +0530

    Comments Are Important

--- TypeScript/lib/index.js
+++ TypeScript/lib/index.js
@@ -1,15 +1,29 @@
 "use strict";
+//== BASICS ==//
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addNumbers = void 0;
+/* -------------------------------------------------------------------------- */
+/*                        First File With Documentation                       */
+/* -------------------------------------------------------------------------- */
+/**
+ * @summary We should always write code with commets because that helps us and
+ * @summary others to easily understand and emplement out code in the projects
+ */
+/* -------------------------------------------------------------------------- */
+/*                   Better Comments Extenstion Can Be Used                   */
+/* -------------------------------------------------------------------------- */
+// TODO Hello There this is what we gonna do
+// ? This is The info
+// ! This is gonna be deprecated
+// * This is Some Importent Stuff
 /**
  * Add two numbers
  * @function addNumbers Input two numbers
  * @param a first number
  * @param b second number
- * @returns Sum of @param a and @param b
+ * @returns Sum of a and b
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addNumbers = void 0;
 function addNumbers(a, b) {
     return a + b;
 }
 exports.addNumbers = addNumbers;
-//# sourceMappingURL=index.js.map
--- TypeScript/src/new.ts
+++
@@ -1,3 +0,0 @@
-import {addNumbers} from "./index"
-
-addNumbers(10,20)
