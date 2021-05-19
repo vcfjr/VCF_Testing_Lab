ed227a1da67915b94c49429fb42c71808fdc0929
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-19 21:33:52 +0530

    Lexical scope

--- TypeScript/src/Functions.ts
+++ TypeScript/src/Functions.ts
@@ -57,6 +57,45 @@ function contactPeople(method : "email" | "phone", ...people : (HasEmail | HasPh
     }
 }
 
+
+
 contactPeople("email", {name:"name", email:"email"})
 contactPeople("phone", {name:"name", phone:1020})
 contactPeople("email", {name:"name", phone:1020})
+
+
+/* -------------------------------------------------------------------------- */
+/*    the lexical scope (this) of a function is part of its signature    */
+/* -------------------------------------------------------------------------- */
+
+function sendMessage(
+  this: HasEmail & HasPhoneNumber,
+  preferredMethod: "phone" | "email"
+) {
+  if (preferredMethod === "email") {
+    console.log("sendEmail");
+    sendMail(this);
+  } else {
+    console.log("sendTextMessage");
+    sendTextMessage(this);
+  }
+}
+const c = { name: "Mike", phone: 3215551212, email: "mike@example.com" };
+
+function invokeSoon(cb: () => any, timeout: number) {
+  setTimeout(() => cb.call(null), timeout);
+}
+
+// 🚨 this is not satisfied
+// invokeSoon(() => sendMessage("email"), 500);
+
+// ✅ creating a bound function is one solution
+const bound = sendMessage.bind(c, "email");
+invokeSoon(() => bound(), 500);
+// 
+// ✅ call/apply works as well
+invokeSoon(() => sendMessage.apply(c, ["phone"]), 500);
+function sendTextMessage(arg0: HasEmail & HasPhoneNumber) {
+    throw new Error("Function not implemented.")
+}
+
