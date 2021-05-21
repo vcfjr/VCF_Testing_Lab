b9e71279318741452050844c16987ec51a6d5d98
ankit <ac2000choudhary@gmail.com>
2021-05-21 16:11:35 +0530

    Upoaded Multiple Files

--- Backend-server-Testing-Lab/index.js
+++ Backend-server-Testing-Lab/index.js
@@ -12,32 +12,52 @@ var azure = require('azure-storage');
 var blobStorage = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=vedikstorage;AccountKey=7OmeGweYiFuBYd+/nzBAj3chFHo0vycwFc95s2yfbYpDOEbVPEnHY1lI5KWZms2G7O1//8F1JmGt2jcueom8jg==;EndpointSuffix=core.windows.net");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // For Cloud Storage
-app.post('/upload',multipartMiddleware,(req, res) => {
+app.post('/upload', multipartMiddleware, async (req, res) => {
     console.log(req.body)
-    
-    for(var i = 0; i<req.files.file.length; i++)
-    {
+
+
+
+    for (var i = 0; i < req.files.file.length; i++) {
         // let upload = "";
         // console.log(req.files.file[i].name)
         var name = req.files.file[i].name;
-        blobStorage.createBlockBlobFromLocalFile('imagecontainer', name , req.files.file[i].path, function(err, result, response){
-            if(!err){
+        blobStorage.createBlockBlobFromLocalFile('imagecontainer', name, req.files.file[i].path,  (err, result, response)=> {
+            if (!err) {
+                // console.log() blobStorage.getUrl('imagecontainer', name)
+
                 console.log(blobStorage.getUrl('imagecontainer', name))
-                // res.send({
-                //     'message': 'File uploaded successfully.'
-                // })
             }
-            else{
+            else {
                 console.log(err);
             }
-        });
+        })
+
+        if(i === req.files.file.length - 1)
+        {
+            console.log("status")
+            res.send({
+                'message': 'File uploaded successfully.'
+            });
+
+        }
+        
+        console.log(blobStorage.getUrl('imagecontainer', name))
     }
 
     
+
+    // res.send({
+    //     'message': 'File uploaded successfully.'
+    // })
+
+
+    // console.log(url)
+
+
 });
 
 
@@ -56,6 +76,6 @@ app.get('/upload', (req, res) => {
 });
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('listening on port ' + port);
 });
