4b25f2c53a5652a02d383605ca106679f11e5f5a
Himanshu Jangid <iamahalofan@gmail.com>
2021-06-05 22:10:37 +0530

    another test and result

--- Backend-server-Testing-Lab/async_behaviour.js
+++ Backend-server-Testing-Lab/async_behaviour.js
@@ -2,25 +2,54 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res)=>{
+//Test 1 Code
+// app.get("/", (req, res) => {
+//     //we have not addded any time related functions here, we just observed that gap b/w two responses
+//     setTimeout(function () { console.log("hello"); }, 4000)
+//     res.send("requested");
+
+// })
+
+//Test 2 Code
+app.get("/", (req, res) => {
     //we have not addded any time related functions here, we just observed that gap b/w two responses
-    setTimeout(function(){console.log("hello"); res.send("requested");}, 4000)
-    
+    setTimeout(function () { console.log("hello"); res.send("requested"); }, 4000)
+
 })
 
-app.listen(3000 || process.env.PORT, ()=>
-{
+app.listen(3000 || process.env.PORT, () => {
     console.log("listening...");
 })
 
-
 /* -------------------------------------------------------------------------- */
-/*                                    Test                                    */
+/*                                    Test  1                                  */
 /* -------------------------------------------------------------------------- */
 
 /**
  * This Test was done in the following steps
- * 1. One request is made 
+ * 1. One request is made
+ * 2. It is fixed that the console log for this request will come atleast after 4 second
+ * 3. Meanwhile another request is made before the console log for previous request come
+ * 4. Time gap b/w two console logs is observed
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                   RESULT                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Time delay b/w two console logs is equal to the time delay b/w each request send
+ * hense nodejs takes one request and work on it and in this time period it can also
+ * process other requests as well
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                    Test  2                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * This Test was done in the following steps
+ * 1. One request is made
  * 2. It is fixed that the response for this request will come atleast after 4 second
  * 3. Meanwhile another request is made before the respose for previous request come
  * 4. Time gap b/w two responses is observed
@@ -31,7 +60,10 @@ app.listen(3000 || process.env.PORT, ()=>
 /* -------------------------------------------------------------------------- */
 
 /**
- * Time delay b/w two responses is equal to the time delay b/w each request send 
- * hense nodejs takes one request and work on it and in this time period it can also 
- * process other requests as well
+ * Time delay b/w two responses is not equal to the time delay b/w each request send
+ * instead it is sent after for seconds of first request's response sent
+ * hense nodejs takes one request and work on it and in this time period it can not
+ * process other requests
  */
+
+
